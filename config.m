6b8af7835fec70f4c98538aec7d22d638419a16d
Chris Liberatore <cliberatore@tamu.edu>
2016-10-13 10:46:07 -0500

    Utility configuration for SABR

---
+++ config.m
@@ -0,0 +1,15 @@
+%STRAIGHT toolkit
+
+addpath('./toolkits/STRAIGHT/');
+
+%ARCTIC toolkit
+
+addpath('./toolkits/ARCTIC/');
+
+%RASTAMAT toolkit
+
+addpath('./toolkits/rastamat/');
+
+%PSI Utilities
+
+addpath('./toolkits/PSI-utilities/');
