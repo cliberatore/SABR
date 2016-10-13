c55053f7b43ec29e196f16b61822472cf6ab6283
Chris Liberatore <cliberatore@tamu.edu>
2016-10-13 14:20:16 -0500

    Fixed bug in the utterance synthesis code.

--- +sabr/+build/utterance.m
+++ +sabr/+build/utterance.m
@@ -1,39 +1,45 @@
 function [audio_h, w_out, spec_hat, mfcc_hat] = utterance(src_spkr, tgt_spkr, audio, sr, varargin)
 
+if(size(src_spkr.centroids,2) ~= size(tgt_spkr.centroids,2))
+    error('Source and target speaker have different number of centroids.');
+end
+
 if(size(src_spkr.centroids,1) ~= size(tgt_spkr.centroids,1))
     error('Source and target speaker have different number of features.');
 end
 
 p = inputParser;
 
-defaultSynthesis = 'STRAIGHT';
 defaultSpectralUpdate = 1;
-defaultBasis     = 2:24;
-defaultLambda    = 0.025;
+defaultFrameLength    = 80;
+defaultBasis          = 2:24;
+defaultLambda         = 0.025;
 
-expectedSynthesis = {'STRAIGHT'};
-
+addRequired(p,'src_spkr');
+addRequired(p,'tgt_spkr');
+addRequired(p,'audio',@(x) isnumeric(x) && isvector(x));
+addRequired(p,'sr',@(x) isnumeric(x) && (x>0));
 addOptional(p,'lambda',defaultLambda,@isnumeric);
-addOptional(p,'update',defaultSpectralUpdate,@isnumeric);
+addOptional(p,'updateInterval',defaultSpectralUpdate,@isnumeric);
+addOptional(p,'frameLength',   defaultFrameLength,   @isnumeric);
 addOptional(p,'basis',defaultBasis,@(x) all(isnumeric(x) && x>0));
-addOptional(p,'synthesis',defaultSynthesis, @(x) any(validatestring(expectedSynthesis)));
 
-parse(p,varargin);
+parse(p, src_spkr, tgt_spkr, audio, sr, varargin{:});
 
 basis   = p.Results.basis;
 penalty = p.Results.lambda;
 
 %% Extract source utterance MFCCs
 
-[ src_f0, src_spec, src_ap ] = config.load_straight( audio, sr, 0 );
+[ src_f0, src_spec, src_ap ] = cache.load_straight( audio, sr, varargin{:} );
 
 src_mfcc = spectrum.spec2mfcc(src_spec, sr, 24, 0);
 
 %% Build SABR representation and estimate target speaker spectrum
 
-w_out    = sabr.build.weights(src_spkr, src_mfcc, penalty, basis);
-mfcc_hat = [src_mfcc(1,:); tgt_spkr.centroids(bases,:) * w_out];
-spec_hat = spectrum.invmfcc(mfcc_hat(1:24,:),u.sr,size(n3sgram,1));
+w_out    = sabr.build.weights(src_spkr.centroids, src_mfcc, penalty, basis);
+mfcc_hat = [src_mfcc(1,:); tgt_spkr.centroids(basis,:) * w_out];
+spec_hat = spectrum.invmfcc(mfcc_hat(1:24,:),sr,size(src_spec,1));
 adj_f0   = vc.adj_f0(src_f0, tgt_spkr.f0_mean, tgt_spkr.f0_std);
 
 %% reconstruct the audio
